09b428589492e7238d36ad4e7a5dc4466989c31b
hnmht <cnmht369@gmail.com>
2025-11-20 11:42:26 +0800

    Comlete About Page

--- src/pages/protected/about/about.jsx
+++ src/pages/protected/about/about.jsx
@@ -1,18 +1,21 @@
 import { useState, useEffect } from "react";
+import { useTranslation } from "react-i18next";
+import { Link } from "@mui/material";
 import { Divider } from "../../../component/ScMui/ScMui";
 import PageTitle from "../../../component/PageTitle/PageTitle";
-import { Typography,Box } from "@mui/material";
+import { Typography, Box } from "@mui/material";
 import { reqPubSysInfo } from "../../../api/pub";
 import { displayName, backendName, author, version } from "../../../constants";
-
+// System Information
 const About = () => {
     const [appInfo, setAppInfo] = useState(undefined);
+    const { t } = useTranslation();
     useEffect(() => {
         async function initAppInfo() {
             const res = await reqPubSysInfo();
             let newInfo = undefined;
             if (res.status) {
-                newInfo = res.data.data;
+                newInfo = res.data;
             }
             setAppInfo(newInfo);
         }
@@ -20,26 +23,29 @@ const About = () => {
     }, []);
 
     return (<>
-        <PageTitle pageName="关于" />
+        <PageTitle pageName={t("MenuAbout")} displayHelp={false} helpUrl="#" />
         <Divider my={2} />
         <Box>
             <Typography variant="h3" gutterBottom mt={4}>{displayName}</Typography>
-            <Typography variant="h5" gutterBottom>版本:  {version}</Typography>
-            <Typography variant="h5" gutterBottom>作者:  {author}</Typography>
+            <Typography variant="h5" gutterBottom>{t("version")}:  {version}</Typography>
+            <Typography variant="h5" gutterBottom>{t("author")}:  {author}</Typography>
             {appInfo !== undefined
                 ? <>
-                    <Typography variant="h3" gutterBottom mt={4} >{backendName}</Typography>
-                    <Typography variant="h5" gutterBottom>DBID:  {appInfo.dbid}</Typography>
-                    <Typography variant="h5" gutterBottom>DBVersion:  {appInfo.dbversion}</Typography>
-                    <Typography variant="h5" gutterBottom>版本:  {appInfo.serversoft.scserverversion}</Typography>
-                    <Typography variant="h5" gutterBottom>作者:  {appInfo.serversoft.author}</Typography>
-                    <Typography variant="h3" gutterBottom mt={4}>使用许可</Typography>
-                    <Typography variant="h5" gutterBottom >授权给:   {appInfo.organization.organizationname}</Typography>
-                    <Typography variant="h5" gutterBottom>最大用户数:  {appInfo.serversoft.maxusernumber}</Typography>
-                    <Typography variant="h5" gutterBottom >组织ID:   {appInfo.organization.organizationid}</Typography>
+                    <Divider my={2} />
+                    <Typography variant="h3" gutterBottom mt={4}>{backendName}</Typography>
+                    <Typography variant="h5" gutterBottom>{t("dbID")}:  {appInfo.dbID}</Typography>
+                    <Typography variant="h5" gutterBottom>{t("dbVersion")}:  {appInfo.dbVersion}</Typography>
+                    <Typography variant="h5" gutterBottom>{t("version")}:  {appInfo.serverSoft.scServerVersion}</Typography>
+                    <Typography variant="h5" gutterBottom>{t("author")}:  {appInfo.serverSoft.author}</Typography>
+
                 </>
                 : null
-            }           
+            }
+            <Divider my={2} />
+            <Typography variant="h3" gutterBottom mt={4}>{t("openSourceLicense")}</Typography>
+            <Typography variant="h5" gutterBottom>
+                <Link href="https://www.gnu.org/licenses/" target="_blank">GNU GPL 3.0 License</Link>
+            </Typography>
         </Box>
     </>);
 };
